02376d11147476794743ffdc8fe53c5a15323e19
Giorgi Charashvili <giorgirock@hotmail.com>
2023-04-07 14:58:20 +0400

    finished building

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,138 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+
+const generateUsers = () => {
+  return [
+    {
+      id: 1,
+      name: 'Alex Johnson',
+      age: 25,
+    },
+    {
+      id: 2,
+      name: 'Benjamin Lee',
+      age: 32,
+    },
+    {
+      id: 3,
+      name: 'Catherine Davis',
+      age: 28,
+    },
+    {
+      id: 4,
+      name: 'Daniel Brown',
+      age: 30,
+    },
+    {
+      id: 5,
+      name: 'Ella Martinez',
+      age: 27,
+    },
+    {
+      id: 6,
+      name: 'Finnegan Campbell',
+      age: 31,
+    },
+    {
+      id: 7,
+      name: 'Grace Taylor',
+      age: 29,
+    },
+    {
+      id: 8,
+      name: 'Harrison Mitchell',
+      age: 26,
+    },
+    {
+      id: 9,
+      name: 'Isabelle Robinson',
+      age: 33,
+    },
+    {
+      id: 10,
+      name: 'Jacob Perez',
+      age: 24,
+    },
+  ];
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState<
+    { id: number; name: string; age: number }[]
+  >([]);
+  useEffect(() => {
+    setUsers(generateUsers());
+  }, []);
+  useEffect(() => {
+    document.title = `${users.length} users Left`;
+  }, [users]);
 
+  const deleteRandomUser = () => {
+    const randomIndex = Math.floor(Math.random() * users.length);
+    setUsers((prevUsers) =>
+      prevUsers.filter((_, index) => index !== randomIndex)
+    );
+  };
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <div className='App'>
+        <div>
+          <h1
+            style={{
+              textAlign: 'center',
+              marginTop: '30px',
+              marginBottom: '50px',
+            }}
+          >
+            Hi
+          </h1>
+          <div
+            style={{
+              display: 'flex',
+              flexWrap: 'wrap',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            {users.map((user, i) => (
+              <div
+                key={i}
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  backgroundColor: '#3e3e3e',
+                  alignItems: 'center',
+                  justifyContent: 'space-evenly',
+                  width: '340px',
+                  height: '100px',
+                  margin: '15px 40px',
+                  borderRadius: '5px',
+                }}
+              >
+                <h2>{user.name}</h2>
+                <h2>{user.age}</h2>
+              </div>
+            ))}
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <button
+              style={{
+                marginTop: '55px',
+                width: '250px',
+                height: '75px',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                textAlign: 'center',
+              }}
+              onClick={deleteRandomUser}
+            >
+              <h3>Delete Random User</h3>
+            </button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
